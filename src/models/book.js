4ed6653bc375200b160d1bcb3380e6cf9dd45935
Shahid Hussain <sh604780@gmail.com>
2025-05-27 00:35:03 +0530

    Update book.js

--- src/models/book.js
+++ src/models/book.js
@@ -1,13 +1,25 @@
 const mongoose = require("mongoose")
 
-const bookSchema = new mongoose.Schema({
-	title: String,
-	author: String,
-	genre: String,
-	createdBy: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "User",
+const bookSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		author: {
+			type: String,
+			required: true,
+		},
+		genre: {
+			type: String,
+			required: true,
+		},
+		createdBy: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+		},
 	},
-})
+	{ timestamps: true }
+)
 
 module.exports = mongoose.model("Book", bookSchema)
